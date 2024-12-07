f583f73128cecff77ec708cf7078f5a7f9ab2ba7
NgTPhu <ngth.phu2608@gmail.com>
2024-12-07 16:23:44 +0700

    colabEvent

--- src/pages/Colab/Content/Event/index.jsx
+++ src/pages/Colab/Content/Event/index.jsx
@@ -143,9 +143,9 @@ const Event = () => {
   };
 
   // Xử lý sửa sự kiện
-  const handleEditEvent = () => {
-    setShowEditEvent(true);
-  };
+  // const handleEditEvent = () => {
+  //   setShowEditEvent(true);
+  // };
 
   const handleCloseEditEvent = () => {
     setShowEditEvent(false);
@@ -245,136 +245,25 @@ const Event = () => {
             className="rounded-md border-2 p-2"
           />
         </div>
-        <table className="mt-4 w-full">
-          <thead>
-            <tr>
-              <th className="text-left">#</th>
-              <th className="text-left">Thời gian</th>
-              <th className="text-left">Tên sự kiện</th>
-              <th className="text-left">Địa điểm</th>
-              <th className="text-left">Đơn vị</th>
-              {/* checkin là switch checkbox input */}
-              <th className="text-left">Checkin</th>
-              {/* Hiển thị là switch checkbox input */}
-              <th className="text-left">Hiển thị</th>
-              {/* Số lượng tham gia */}
-              <th className="text-left">Số lượng tham gia</th>
-              <th className="text-left">Hành động</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableData.map((item) => (
-              <tr
-                key={item.id}
-                className="border-y-2 border-gray-200 hover:bg-gray-100"
-              >
-                <td>{item.id}</td>
-                <td>{dayjs(item.time).format("DD-MM-YYYY")}</td>
-                <td>
-                  <div>
-                    {item.title}
-                    <div className="mt-2 flex space-x-1">
-                      {item.topic.map((topic) => (
-                        <span
-                          key={topic.id}
-                          className="rounded-full bg-gray-500 p-0 px-1 text-[12px] text-white"
-                        >
-                          {topic.name}
-                        </span>
-                      ))}
-                    </div>
-                  </div>
-                </td>
-                <td>{item.location}</td>
-                <td>{item.hostBy}</td>
-                {/* checkin là switch checkbox input customize lại kiểu checkbox là nút switch bật tắt */}
-                <td>
-                  <input
-                    className="h-7 w-14"
-                    type="checkbox"
-                    checked={item.allowCheckin}
-                  />
-                </td>
-                {/* Hiển thị là switch checkbox input */}
-                <td>
-                  <input
-                    className="h-7 w-14 rounded-full"
-                    type="checkbox"
-                    checked={!item.hidden}
-                  />
-                </td>
-                {/* Số lượng checkin / tổng số người đăng ký */}
-                <td>
-                  <label className="rounded-md bg-blue-500 p-1 text-white transition-all duration-300 ease-in-out hover:bg-blue-700">
-                    {item.checkin}
-                  </label>
-                  <label className="ml-2 rounded-md bg-red-500 p-1 text-white transition-all duration-300 ease-in-out hover:bg-red-700">
-                    {item.register}
-                  </label>
-                </td>
-                <td>
-                  <button
-                    onClick={handleEditEvent}
-                    className="rounded-md bg-blue-500 p-1 text-white transition-all duration-300 ease-in-out hover:bg-blue-700"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-6"
+        <div className="mt-4 grid grid-cols-4 gap-4">
+                    {tableData.map((event) => (
+                    <Link
+                      key={event.id}
+                      to={`/colab/event/${event.title}`}
+                      className="rounded-lg bg-gradient-to-r from-cyan-400 to-blue-400 p-4 text-white shadow-lg transition-transform duration-300 hover:scale-105"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                      />
-                    </svg>
-                  </button>
-                  <button className="ml-2 rounded-md bg-green-500 p-1 text-white hover:bg-green-700">
-                    <Link to={`/colab/event/${item.id}`}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="size-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Zm3.75 11.625a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z"
-                        />
-                      </svg>
+                      <h2 className="text-lg font-semibold uppercase">{event.title}</h2>
+                      <p className="mt-2 text-sm">
+                        <span className="font-semibold">📅 Ngày: </span>
+                        {event.time.split(" ")[0]}
+                      </p>
+                      <p className="mt-1 text-sm">
+                        <span className="font-semibold">📍 Địa điểm: </span>
+                        {event.location}
+                      </p>
                     </Link>
-                  </button>
-
-                  {/* <button
-                    onClick={handleDeleteEvent}
-                    className="ml-2 rounded-md bg-red-500 p-1 text-white transition-all duration-300 ease-in-out hover:bg-red-700"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="size-6"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </button> */}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                  ))}
+        </div>
         <div className="mt-4 flex">
           {page > 1 && (
             <button
